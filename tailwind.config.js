0d17c30ad1adce7462514e4de4c37700f71f01b3
Patrick Ma <patrickma6199@gmail.com>
2024-10-05 17:38:49 -0700

    Added NavBar and trying to configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'dark-blue': '#022859',
+      'light-blue': '#1a7dd8',
+      'lemon': '#ffc208',
+      'orange': '#f37600',
+      'dark-orange': '#f34604',
+    },
+    fontFamily: {
+      mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [],
+}
+
