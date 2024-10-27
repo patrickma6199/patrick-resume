17b1b85cd7a159c930acd50f9cb557edf721a8ee
patrickma6199 <patrickma6199@gmail.com>
2024-10-26 17:03:14 -0700

    trying something new

--- deploy/webHooks/git-pull.sh
+++ deploy/webHooks/git-pull.sh
@@ -5,6 +5,6 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
 if [ "$LOCAL" != "$REMOTE" ]; then
     git pull origin master
-    timeout 5s yarn
-    timeout 5s docker compose up -d --build
+    yarn
+    docker compose up -d --build
 fi
